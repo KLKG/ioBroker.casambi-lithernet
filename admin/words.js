ecdcb180942979ab79d4d9076a4994cdf46605a7
Friedrich Engel <engel@fengel.info>
2023-04-16 19:23:41 +0200

    template comit

---
+++ admin/words.js
@@ -0,0 +1,41 @@
+/*global systemDictionary:true */
+'use strict';
+
+systemDictionary = {
+    'casambi-lithernet adapter settings': {
+        'en': 'Adapter settings for casambi-lithernet',
+        'de': 'Adaptereinstellungen für casambi-lithernet',
+        'ru': 'Настройки адаптера для casambi-lithernet',
+        'pt': 'Configurações do adaptador para casambi-lithernet',
+        'nl': 'Adapterinstellingen voor casambi-lithernet',
+        'fr': "Paramètres d'adaptateur pour casambi-lithernet",
+        'it': "Impostazioni dell'adattatore per casambi-lithernet",
+        'es': 'Ajustes del adaptador para casambi-lithernet',
+        'pl': 'Ustawienia adaptera dla casambi-lithernet',
+        'zh-cn': 'casambi-lithernet的适配器设置'
+    },
+    'option1': {
+        'en': 'option1',
+        'de': 'Option 1',
+        'ru': 'Опция 1',
+        'pt': 'Opção 1',
+        'nl': 'Optie 1',
+        'fr': 'Option 1',
+        'it': 'opzione 1',
+        'es': 'Opción 1',
+        'pl': 'opcja 1',
+        'zh-cn': '选项1'
+    },
+    'option2': {
+        'en': 'option2',
+        'de': 'Option 2',
+        'ru': 'вариант 2',
+        'pt': 'opção 2',
+        'nl': 'Optie 2',
+        'fr': 'Option 2',
+        'it': 'opzione 2',
+        'es': 'opcion 2',
+        'pl': 'Opcja 2',
+        'zh-cn': '选项2'
+    }
+};
